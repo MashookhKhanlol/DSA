bfb8f5bbc2cc47dccce86847fc046df4f546315d
MashookhKhanlol <113329651+MashookhKhanlol@users.noreply.github.com>
2023-01-07 12:49:57 +0530

    Add files via upload

---
+++ Assignment4/DetectACycle.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+    if ( NULL == head )
+        return false;
+    
+    Node *temp = head;
+    Node **visit = new Node *[200];
+    int num=0;
+    while (temp )
+    {
+        for (int i=0; i<num; i++)
+        {
+            if ( visit[i] == temp )
+            {
+                delete [] visit;
+                return true;
+            }
+        }
+        visit[num++] =temp;
+        
+            temp = temp->next;
+    }
+    return false;
+}
